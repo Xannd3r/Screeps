fc5174be831c528a0278afa82975ac2d771a0da4
Ammar <totumspiretfoxx@gmail.com>
2024-01-26 11:49:31 -0700

    Update role.builder.js

--- Creeps Code/role.builder.js
+++ Creeps Code/role.builder.js
@@ -3,6 +3,21 @@ var roleBuilder = {
     /** @param {Creep} creep **/
 
     run: function(creep) {
+        
+        // Automatically upkeep structures
+        var targets = creep.room.find(FIND_STRUCTURES, {
+            filter: (structure) => structure.hits < structure.hitsMax
+        });
+
+        if (targets.length > 0) {
+            if (creep.repair(targets[0]) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(targets[0], { visualizePathStyle: { stroke: '#ffffff' } });
+            }
+        } else {
+        
+        
+        
+        
 
 	    if(creep.memory.building && creep.store[RESOURCE_ENERGY] == 0) {
             creep.memory.building = false;
@@ -25,6 +40,7 @@ var roleBuilder = {
 	        var sources = creep.room.find(FIND_SOURCES);
             if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
                 creep.moveTo(sources[0], {visualizePathStyle: {stroke: '#ffaa00'}});
+            }
             }
 	    }
 	}
